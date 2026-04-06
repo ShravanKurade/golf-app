59a20d5dd7dc7b41181cd22e79c230c71a64636f
Shravan Kurade <shravanakurade2005@gmail.com>
2026-04-07 02:23:51 +0530

    Upgrade UI

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -517,16 +517,6 @@ const totalDonated = draws.reduce((sum, d) => {
     Logout
   </button>
 </div>
-
-  <button
-    className="bg-gradient-to-r from-pink-500 to-purple-500 text-white px-4 py-2 rounded-xl mb-3 hover:scale-105 hover:shadow-lg transition duration-300"
-    onClick={async () => {
-      await supabase.auth.signOut();
-      navigate("/login");
-    }}
-  >
-    Logout
-  </button>
       </motion.div>
     </div>
   );
