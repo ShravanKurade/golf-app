911e9cf88051b7aa4e4c9dd5f763a0c7e08ca373
Shravan Kurade <shravanakurade2005@gmail.com>
2026-04-07 18:16:19 +0530

    Upgrade UI

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -194,7 +194,7 @@ function Dashboard() {
   const amount = plan === "monthly" ? 9900 : 99900;
 
   const options = {
-    key: "rzp_test_SKs8rO9rZ7O1Uj", // 🔑 tera key
+    key: "rzp_test_SKs8rO9rZ7O1Uj", // 🔑 key
     amount: amount,
     currency: "INR",
     name: "Golf Charity App",
@@ -222,6 +222,8 @@ function Dashboard() {
     })
     .eq("id", user.id);
 
+    setSubscription("active");
+    setSubscriptionEnd(end);
   // ✅ EMAIL SEND CODE
   const templateParams = {
     to_email: user.email,
@@ -436,19 +438,20 @@ const totalDonated = draws.reduce((sum, d) => {
 
         {/* SUBSCRIPTION */}
         <button
-          className={`w-full px-4 py-2 rounded-xl text-white mt-3 transition duration-300 ${
-  subscription === "active"
-    ? "bg-green-500 hover:bg-green-600 hover:scale-105 active:scale-95"
-    : "bg-gradient-to-r from-pink-500 to-purple-500 hover:scale-105 hover:shadow-lg active:scale-95"
-}`}
-          onClick={handlePayment}
-        >
-          {subscription === "active"
-            ? "✅ Premium Active"
-            : plan === "monthly"
-              ? "Buy Monthly ₹99 💳"
-              : "Buy Yearly ₹999 💳"}
-        </button>
+  disabled={subscription === "active"}   // ✅ IMPORTANT
+  className={`w-full px-4 py-2 rounded-xl text-white mt-3 transition duration-300 ${
+    subscription === "active"
+      ? "bg-green-500 cursor-not-allowed"
+      : "bg-gradient-to-r from-pink-500 to-purple-500 hover:scale-105 hover:shadow-lg active:scale-95"
+  }`}
+  onClick={handlePayment}
+>
+  {subscription === "active"
+    ? "✅ Premium Active"
+    : plan === "monthly"
+      ? "Buy Monthly ₹99 💳"
+      : "Buy Yearly ₹999 💳"}
+</button>
 
         {/* PLAN DISPLAY */}
         <p className="text-white text-sm mt-1">
